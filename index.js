80bb4683542677b3a857baf1345812c31bb41c47
Benjamin Esh <beshdevelopment@gmail.com>
2020-02-03 14:13:44 -0600

    init commit
    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var myArgs = process.argv.slice(2);
+
+const fizzBuzz = (num, multiOne, multiTwo = 0) => {
+    const bizzArray = [];
+    for(let i = 1; i <= num; i++){
+        if(i % multiOne === 0 && i % multiTwo === 0){
+            bizzArray.push("FIZZBUZZ!!!!");
+        } else if(i % multiOne === 0){
+            bizzArray.push("Fizz!");
+        } else if (i % multiTwo === 0){
+            bizzArray.push("Buzz!");
+        } else {
+            bizzArray.push(i);
+        }
+    }
+    return bizzArray;
+}
+
+console.log(fizzBuzz(myArgs[0], myArgs[1], myArgs[2]));
